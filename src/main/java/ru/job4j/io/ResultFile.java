011c9ec35393c07c457e1c20ed17b3c13de5178d
ruslandavydov <Guinness2374637>
2021-04-07 13:48:39 +0300

    0.1. FileOutputStream. [#498781]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+
+    public static void main(String[] args) {
+        int two = 2;
+        int four = 4;
+        int twoTimesFour = two * four;
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            out.write("Two times Four equals ".getBytes());
+            out.write(String.valueOf(twoTimesFour).getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
