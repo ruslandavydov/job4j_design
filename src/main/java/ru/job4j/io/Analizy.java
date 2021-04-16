26928f37ab13c8ef233b6177a01701fb0e6a723a
ruslandavydov <Guinness2374637>
2021-04-16 19:14:45 +0300

    2. Анализ доступности сервера. [#498788]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -6,31 +6,25 @@ import java.util.List;
 
 public class Analizy {
     private String start;
+    private String finish;
 
     public void unavailable(String source, String target) {
-        List<String> lines = new ArrayList<>();
-        StringBuilder rsl = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            List<String> lines = new ArrayList<>();
+            StringBuilder rsl = new StringBuilder();
             while (in.ready()) {
                 if (in.readLine().startsWith("400") || in.readLine().startsWith("500")) {
                     start = in.readLine().split(" ")[1];
                 } else if (in.readLine().startsWith("200") || in.readLine().startsWith("300")) {
-                    String finish = in.readLine().split(" ")[1];
-                    rsl.append(String.format("%s;%s;%s", start, finish, System.lineSeparator()));
-                    lines.add(rsl.toString());
-                    rsl = new StringBuilder();
+                    finish = in.readLine().split(" ")[1];
                 }
+                rsl.append(String.format("%s;%s", start, finish));
+                lines.add(rsl.toString());
+                rsl = new StringBuilder();
             }
-            writeFile(lines, target);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void writeFile(List<String> lines, String target) {
-        try (BufferedWriter out = new BufferedWriter(new FileWriter(target))) {
             for (String line : lines) {
-                out.write(line);
+                out.println(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
