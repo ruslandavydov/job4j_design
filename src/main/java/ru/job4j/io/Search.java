48383390a8cd4a027e32c2ebd5c2a1d41d0b0562
ruslandavydov <Guinness2374637>
2021-04-23 02:59:40 +0300

    4.1. Сканирование файловой системы. [#498778]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -2,11 +2,19 @@ package ru.job4j.io;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Search {
 
     public static void main(String[] args) throws IOException {
         Path start = Paths.get("/Users/Nastya/projects");
-        Files.walkFileTree(start, new SearchFiles());
+        search(start, path -> path.toFile().getName().endsWith(".java")).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
     }
 }
--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -5,13 +5,27 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class SearchFiles extends SimpleFileVisitor<Path> {
+    private Predicate<Path> condition;
+    private List<Path> paths;
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    public List<Path> getPaths() {
+        return paths;
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (file.toString().endsWith(".java")) {
-            System.out.println(file.toAbsolutePath());
+        paths = new ArrayList<>();
+        if (condition != null) {
+            paths.add(file.toAbsolutePath());
         }
         return FileVisitResult.CONTINUE;
     }
