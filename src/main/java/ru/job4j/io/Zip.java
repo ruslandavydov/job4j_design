19efddae7ab3adf04f715cc076ebcbad25d67a89
ruslandavydov <Guinness2374637>
2021-05-04 12:36:50 +0300

    5.2. Архивировать проект

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public List<Path> seekBy(String root, String ext) throws IOException {
+        return Search.search(Path.of(root), x -> !x.endsWith(ext));
+    }
+
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(String.valueOf(source)));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(String.valueOf(source)))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Zip zip = new Zip();
+        new Zip().packFiles(
+                zip.seekBy("/Users/Nastya/projects/job4j_design", "java"),
+                new File("project.zip"));
+    }
+}
